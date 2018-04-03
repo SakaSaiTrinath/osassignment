f56f29018c66e1093775d261e0c7d24983295139
Saka Sai Trinath <sst.trinath@gmail.com>
2018-04-03 18:54:59 +0530

    Update pidmanager.h

--- pidmanager.h
+++ pidmanager.h
@@ -31,18 +31,13 @@ int allocate_pid()
       return i+100;        //returning pid
     }
   }
-  return 1;                //if pids are not free, 1 is returned indicating the same.
+  return 1;                //if pids are not free, 1 is returned.
 }
 
-int release_pid(int p)
+void release_pid(int p)
 {
   int pid=p-100;            //taking pid from parameter.
   pid_status[pid]=0;        //changing pid status.
-  
-  if(pid_status[pid]==0)
-    return 0;               //return status 0 to indicate pid is released successfully.
-  else
-    return 1;               //return 1 if unsuccessful.  
 }
 
 void display()
