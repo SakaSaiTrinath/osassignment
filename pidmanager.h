fbcb42a0e0673c24b937ca91766dea5c88fe2600
Saka Sai Trinath <sst.trinath@gmail.com>
2018-04-03 18:35:45 +0530

    Update pidmanager.h

--- pidmanager.h
+++ pidmanager.h
@@ -17,6 +17,7 @@ int allocate_map()        //initializing pid statuses
   {
     pid_status[i]=0;
   }
+  return 0;               //return 0 indicating statuses have initialized.
 }
 
 int allocate_pid()
@@ -30,14 +31,18 @@ int allocate_pid()
       return i+100;        //returning pid
     }
   }
-  return 0;                //if pids are not free, 0 is returned indicating the same.
+  return 1;                //if pids are not free, 1 is returned indicating the same.
 }
 
 int release_pid(int p)
 {
-  int pid=p-100;            //taking pid from parameter
-  pid_status[pid]=0;        //changing pid status
-  return 1;                 //return status 1 to indicate success
+  int pid=p-100;            //taking pid from parameter.
+  pid_status[pid]=0;        //changing pid status.
+  
+  if(pid_status[pid]==0)
+    return 0;               //return status 0 to indicate pid is released successfully.
+  else
+    return 1;               //return 1 if unsuccessful.  
 }
 
 void display()
