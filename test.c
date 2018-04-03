286677e1a3db61ca29f214d6b734a3ec85dee9bd
Saka Sai Trinath <sst.trinath@gmail.com>
2018-04-03 18:12:32 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,46 +1,99 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<time.h>
 #include "pidmanager.h"
 
 struct proc
 {
+  int sno;
   int pid;
-  int sleep_time;
+  int sl_time;
 };
 
-void pid_check(int pid)
+int pid_check(int pid)
 {
   if(pid>=100)
   {
-    printf("\nPid assigned successfully...");
-    printf("\nPid is: %d",pid);
+    printf("\nPid %d assigned successfully...\n",pid);
+    return 1;
+    //printf("\nPid is: %d",pid);
   }
   else if(pid==0)
   {
     printf("\npid allocation failed...\n");
+    return 0;
   }
   else
   {
     printf("\nSomething went wrong... pid: %d is returned...",pid);
+    return 0;
+  }
+}
+
+int set_sleep_time()
+{
+  //srand(2);
+  return rand()%10;
+}
+
+void *thread_fun(void *pro)
+{
+  struct proc *p = (struct proc *) pro;
+  printf("\n%d: Fetching pid...\n",p->sno);
+  p->pid = alocate_pid();
+  
+  while(pid_check(p->pid)==0)
+  {
+    sleep(2);
+    printf("\n%d: Fetching pid...\n",p->sno);
+    p->pid = allocate_pid();
+  }
+  display();
+  
+  p->sl_time = set_sleep_time();
+  printf("\n%d: pid = %d,sl_time = %d\n",p->sno,p->pid,p->sl_time);
+  printf("\n%d - %d: Entered Sleep time...\n",p->sno,p->pid);
+  sleep(p->sl_time);
+  printf("\n%d - %d: Exited Sleep time...\n",p->sno,p->pid);
+  int pos = release_pid(p->pid);
+  display();
+  
+  if(pos==0)
+  {
+    printf("\n%d - %d: pid released...\n",p->sno,p->pid);
+  }
+  else
+  {
+    printf("\n%d - %d: problem in releasing pid...\n",p->sno,p->pid);
   }
 }
 
 int main()
 {
-  struct proc p1,p2;
+  int i, no_of_threads;
+  printf("Enter no of threads: ");
+  scanf("%d",&no_of_threads);
+  printf("...program getting stared...\n");
+  
   allocate_map();
-  printf("\nAfter allocate_map()\n");
-  display();
-  p1.pid = allocate_pid();
-  pid_check(p1.pid);
-  p2.pid = allocate_pid();
-  pid_check(p2.pid);
-  pid_check(p2.pid);
-  printf("\nAfter allocate_pid()\n");
-  display();
-  release_pid(p1.pid);
-  printf("\nAfter release_pid()\n");
-  display();
-  printf("\nProgram terminated successfully...\n");
+  printf("Map allocated...\n");
+  
+  struct proc pr[no_of_threads];
+  pthread_t pr_threads[no_of_threads];
+  
+  for(i=0;i<no_of_threads;i++)
+  {
+    pr[i].sno = i;
+    pthread_create(&pr_threads[i],NULL,thread_fun,&pr[i]);
+  }
+  
+  for(i=0;i<no_of_threads;i++)
+  {
+    pthread_join(pr_threads[i],NULL);
+  }
+  
+  printf("\n...Program Successfully terminated...\n");
 }
   
   
