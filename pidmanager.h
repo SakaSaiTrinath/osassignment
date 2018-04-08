5d9a7f73918854ae8a81de438b14ef3bbff5256f
Saka Sai Trinath <sst.trinath@gmail.com>
2018-04-08 12:22:10 +0530

    written detailed comments

--- pidmanager.h
+++ pidmanager.h
@@ -1,3 +1,12 @@
+/*
+  This file is a header file which defines the pidmanager.
+  
+  allocate_map() function initializes the pid statuses.
+  allocate_pid() function returns a free pid from the data structure and changes the status of that pid to 1 on calling from a thread, 
+    otherwise i.e., if no pid is free, it returns 1.
+  release_pid() function takes back the pid on calling from the thread. i.e., it changes the pid status of that pid back to 0.
+  display() function shows all the pids which are in use when it is called.
+*/
 
 #ifndef _pidmanager_h
 #define _pidmanager_h
@@ -8,7 +17,7 @@
 #define MAX_PID 1000
 #define ARR_SIZE MAX_PID-MIN_PID+1
 
-int pid_status[ARR_SIZE];
+int pid_status[ARR_SIZE];       //data structure to store or maintain the pid statuses.
 
 int allocate_map()        //initializing pid statuses
 {
@@ -46,7 +55,7 @@ void display()
   printf("\n----------------------------------------");
   for(i=0;i<ARR_SIZE;i++)
   {
-    if(pid_status[i]==1)
+    if(pid_status[i]==1)              //checking for pids which are in use.
     {
       printf("\n[%d] = %d\t",i+100,pid_status[i]);
       c++;
